acb8710540bfe8e961a0c21106b24754dfae22de
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2022-12-08 12:30:51 +0400

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,14 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num == 0)
+{
+    Console.WriteLine("0 ne cutdur nede tek");
+
+}
+else if (num%2==0)
+{
+    Console.WriteLine(num/2);
+}
+else
+{
+    Console.WriteLine(num*2);
+}
