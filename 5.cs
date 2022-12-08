b6a08068138bf108a8b24786969a06fd1cbf6287
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2022-12-08 14:43:41 +0400

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,20 @@
+﻿int age;
+
+do
+{   
+    Console.WriteLine("Yasinizi daxil edin");
+    age = Convert.ToInt32(Console.ReadLine());
+    if (age<1||age>100)
+    {
+        Console.WriteLine("Yasinizi duzgun daxil edin");
+    }
+    
+} while (age<1||age>100);
+if (age<18)
+{
+    Console.WriteLine(18-age +" " + "ile ala bilersiniz");
+}
+else
+{
+    Console.WriteLine("Suruculuk vesiqesini ala bilersiniz");
+}
